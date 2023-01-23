ba0b6136a1ea632c08bbaff416d4cdc818ae5d40
Helmut Hummel <typo3@helhum.io>
2023-01-23 01:04:02 +0100

    Fix website bootstrap navigation and restructure vite build

---
+++ Resources/Private/JavaScript/topwire.js
@@ -0,0 +1,33 @@
+import * as Turbo from '@hotwired/turbo'
+import { morph } from '@alpinejs/morph'
+
+Turbo.session.drive = true
+
+document.addEventListener('turbo:before-fetch-request', async (event) => {
+    event.preventDefault()
+    const headers = event.detail.fetchOptions.headers
+    if (event.target.tagName === 'HTML') {
+        // We are not interested in Turbo drive requests for now
+        event.detail.resume()
+        return
+    }
+    // Link trigger or form submit?
+    const turboFrame = event.target.tagName === 'turbo-frame' ? event.target : event.target.closest('turbo-frame')
+    if (!headers['Turbo-Frame'] || !turboFrame?.dataset?.topwireContext) {
+        event.detail.resume()
+        return
+    }
+    if (turboFrame?.dataset?.topwireMorph && turboFrame?.__topwire?.morphListener === undefined) {
+        const morphListener = (event) => {
+            event.detail.render = (currentElement, newElement) => {
+                morph(currentElement, newElement)
+            }
+        }
+        turboFrame.addEventListener('turbo:before-frame-render', morphListener)
+        turboFrame['__topwire'] = {}
+        turboFrame['__topwire']['morphListener'] = morphListener
+    }
+    headers['Topwire-Context'] = turboFrame.dataset.topwireContext
+    event.detail.fetchOptions.headers = headers
+    event.detail.resume()
+})
