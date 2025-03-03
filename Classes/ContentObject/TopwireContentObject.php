a136247d27a0801832c5945217b7a293980835ec
Sascha Egerer <sascha.egerer@flowd.de>
2025-03-03 22:20:55 +0100

    Add missing request as argument

--- Classes/ContentObject/TopwireContentObject.php
+++ Classes/ContentObject/TopwireContentObject.php
@@ -35,7 +35,7 @@ class TopwireContentObject extends AbstractContentObject
         }
 
         $frameOptions = new FrameOptions();
-        $pageTitle = $this->getTypoScriptFrontendController()->generatePageTitle();
+        $pageTitle = $this->getTypoScriptFrontendController()->generatePageTitle($this->request);
         if ($pageTitle !== '') {
             $frameOptions = new FrameOptions(pageTitle: $pageTitle);
         }
