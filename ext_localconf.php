6f312b146da2ac33da1fa2920b615e561aa31bbe
Thorben Nissen <thorben@webcoast.dk>
2025-12-11 09:37:12 +0100

    [BUGFIX] Make content element wrap work in TYPO3 CMS 13
    * refactor stdWrap hook to event listener
    * remove hook registration in `ext_localconf.php`

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,6 @@ use TYPO3\CMS\Frontend\Typolink\PageLinkBuilder;
 (static function (): void {
     $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_topwire';
     $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_topwire_document';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap']['tx_topwire'] = ContentElementWrap::class;
     if ($GLOBALS['TYPO3_CONF_VARS']['FE']['typolinkBuilder']['page'] !== PageLinkBuilder::class) {
         $GLOBALS['TYPO3_CONF_VARS']['FE']['typolinkBuilder']['overriddenDefault'] = $GLOBALS['TYPO3_CONF_VARS']['FE']['typolinkBuilder']['page'];
     }
